68be35da9ce802463c67180a4dfdd177305cc3fd
eapinto88 <everton.assis096@gmail.com>
2025-07-25 18:50:24 -0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,25 +1,7 @@
-const CACHE_NAME = 'escala-futebol-cache-v1';
-const urlsToCache = [
-  '/',
-  '/index.html',
-  '/style.css',
-  '/script.js',
-  '/icon-192.png',
-  '/icon-512.png'
-];
-
 self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(urlsToCache);
-    })
-  );
+  console.log('Service Worker instalado');
 });
 
 self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
-  );
+  event.respondWith(fetch(event.request));
 });
